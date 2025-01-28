3e77a1fafcc1234a086dd6c7ebb09a667f056879
ErayOnder <erayonder21@ku.edu.tr>
2025-01-28 20:22:13 +0300

    Added high score feature

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -6,15 +6,30 @@ public class LogicScript : MonoBehaviour
 {
     public int playerScore = 0;
     public Text scoreText;
+    public Text highScoreText;
     public GameObject gameOverScreen;
     public AudioSource scoreSound;
     public AudioSource dieSound;
     
+    private int highScore;
+    private BirdScript birdScript;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        highScoreText.text = "High Score: " + highScore.ToString();
+
+        birdScript = GameObject.FindGameObjectWithTag("Bird").GetComponent<BirdScript>();
+    }
+
     public void addScore(int score)
     {
-        playerScore += score;
-        scoreText.text = playerScore.ToString();
-        scoreSound.Play();
+        if (birdScript.isDead == false)
+        {
+            playerScore += score;
+            scoreText.text = playerScore.ToString();
+            scoreSound.Play();
+        }
     }
 
     public void restartGame()
@@ -27,6 +42,13 @@ public class LogicScript : MonoBehaviour
         Debug.Log("Game Over");
         gameOverScreen.SetActive(true);
         dieSound.Play();
+
+        if (playerScore > highScore)
+        {
+            highScore = playerScore;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            highScoreText.text = "High Score: " + highScore.ToString();
+        }
     }
 
 }
